4b5cefadae30e83d4da6aaa8a7911c8581d7ca2e
Christoph Schweppe <info@cschweppe.de>
2020-12-30 17:27:09 +0400

    Update to latest Voyager changes

--- resources/src/tiptap.js
+++ resources/src/tiptap.js
@@ -1,14 +1,14 @@
 import Tiptap from './components/Tiptap';
-Vue.component('tiptap', Tiptap);
+voyager.component('tiptap', Tiptap);
 
 import TiptapBrowse from './components/Formfield/Browse';
-Vue.component('formfield-tiptap-browse', TiptapBrowse);
+voyager.component('formfield-tiptap-browse', TiptapBrowse);
 
 import TiptapEditAdd from './components/Formfield/EditAdd';
-Vue.component('formfield-tiptap-edit-add', TiptapEditAdd);
+voyager.component('formfield-tiptap-edit-add', TiptapEditAdd);
 
 import TiptapRead from './components/Formfield/Read';
-Vue.component('formfield-tiptap-read', TiptapRead);
+voyager.component('formfield-tiptap-read', TiptapRead);
 
 import TiptapBuilder from './components/Formfield/Builder';
-Vue.component('formfield-tiptap-builder', TiptapBuilder);
+voyager.component('formfield-tiptap-builder', TiptapBuilder);
--- src/Tiptap.php
+++ src/Tiptap.php
@@ -5,7 +5,7 @@ namespace Emptynick\Tiptap;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 use Illuminate\View\View;
-use Voyager\Admin\Classes\Formfield;
+use Voyager\Admin\Contracts\Formfields\Formfield;
 use Voyager\Admin\Contracts\Plugins\FormfieldPlugin;
 
 class Tiptap implements FormfieldPlugin
@@ -16,22 +16,9 @@ class Tiptap implements FormfieldPlugin
     public $website = 'https://github.com/emptynick/voyager-tiptap';
     public $version = '1.0.0';
 
-    public function providePublicRoutes(): void
-    {
-        Route::get('tiptap.js', function () {
-            $path = realpath(dirname(__DIR__, 1).'/resources/dist/tiptap.js');
-            $response = response(File::get($path), 200, ['Content-Type' => 'text/javascript']);
-            $response->setSharedMaxAge(31536000);
-            $response->setMaxAge(31536000);
-            $response->setExpires(new \DateTime('+1 year'));
-
-            return $response;
-        })->name('voyager-tiptap');
-    }
-
     public function provideJS(): string
     {
-        return route('voyager.voyager-tiptap');
+        return file_get_contents(realpath(dirname(__DIR__, 1).'/resources/dist/tiptap.js'));
     }
 
     public function getFormfield(): Formfield
