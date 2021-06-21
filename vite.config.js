c74771439eb4c1c8fae1d01c0d6f11e1972cea16
Christoph Schweppe <info@cschweppe.de>
2021-06-21 11:40:38 +0200

    Bunp dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,23 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  build: {
-    cssCodeSplit: true,
-    lib: {
-      entry: 'src/main.js',
-      name: 'Tiptap',
-      formats: ['umd'],
-      fileName: 'tiptap'
-    },
-    rollupOptions: {
-      external: ['vue'],
-      output: {
-        globals: {
-          vue: 'Vue'
+    plugins: [vue()],
+    build: {
+        cssCodeSplit: true,
+        lib: {
+            entry: 'src/main.js',
+            name: 'Tiptap',
+            formats: ['umd'],
+            fileName: 'tiptap'
+        },
+        rollupOptions: {
+            external: ['vue'],
+            output: {
+                globals: {
+                    vue: 'Vue'
+                }
+            }
         }
-      }
     }
-  }
 })
